1d913af75fa25d3acd8b4ea6eaffd84d8824d51b
S M Jagadish <s.m.jagadish@ericsson.com>
2022-03-25 13:25:43 -0400

    Project skeleton for codeplay

---
+++ springbootplay/src/main/java/com/dailytil/springbootplay/SpringbootplayApplication.java
@@ -0,0 +1,13 @@
+package com.dailytil.springbootplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootplayApplication.class, args);
+	}
+
+}
---
+++ springbootplay/src/test/java/com/dailytil/springbootplay/SpringbootplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailytil.springbootplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
