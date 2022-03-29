9a03f03b6b89925bcabda192047519fc026a2e45
S M Jagadish <s.m.jagadish@ericsson.com>
2022-03-29 14:46:55 -0400

    Playground code for testing injection types

--- springbootplay/src/main/java/com/dailytil/springbootplay/SpringbootplayApplication.java
+++ springbootplay/src/main/java/com/dailytil/springbootplay/SpringbootplayApplication.java
@@ -1,13 +1,58 @@
 package com.dailytil.springbootplay;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringbootplayApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootplayApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringbootplayApplication.class, args);
+		Test obj = ctx.getBean(Test.class);
+System.out.println(obj.type+" "+obj.upgradeable+" "+obj.version);
+System.out.println(obj.printInfo());
+obj.updatedep();
+
+	}
+	@Component
+	public class Test{
+		@Value("${deployment.type}")
+		 private  String type;
+		@Value("${deployment.upgradeable}")
+		 private boolean upgradeable;
+		@Value("${deployment.version}")
+		 private final int version = 23;
+		@Autowired
+		public SecondTest st;
+		Test(@Value("${deployment.version}")int a , final SecondTest stt)
+		{
+System.out.println(this.type);
+//st.upgradeable = false;
+		}
+		private boolean printInfo()
+		{
+
+			return st.upgradeable;
+		}
+		private void updatedep()
+		{
+			st.upgradeable = false;
+			System.out.println("updated" + st.upgradeable);
+		}
+
 	}
 
+	@Component
+	public class SecondTest{
+		@Value("${deployment.upgradeable}")
+		private boolean upgradeable;
+		@Value("${deployment.version}")
+		private int version;
+
+	}
 }
